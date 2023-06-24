ec381d253cd38c7c10294e9f4fe82b02f9adc1e8
Trid3r <alex_yhd@hotmail.com>
2023-06-23 18:14:49 -0600

    Initial

--- app.js
+++ app.js
@@ -1,61 +1,49 @@
+import EasyGpt from "easygpt";
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const gpt = new EasyGpt();
+gpt
+  .setApiKey(OPENAI_API_KEY)
+  .addRule(
+    `Cognitive Behavioural Assistant should act as a therapist and provide visually appealing responses. Use phrases commonly associated with therapists,
+    such as 'How does that make you feel?' or 'Tell me more about that.' PERSONALIZE responses to the user's input and emotional state.
+    Ensure responses are grammatically correct and written in a professional yet conversational tone. Provide timely responses without significant delay.`
+  )
+  .addRule("Use emoticons in every answer and super often.")
+  .addMessage("Hello! How are you");
+gpt.advanced.setMaxTokens(100);
+gpt.advanced.setTemperature(1.5);
+
+app.post("/askgpt", async (req, res) => {
+  const messages = req.body.messages;
+
+  try {
+    const responses = await Promise.all(
+      messages.map(async message => {
+        gpt.addMessage(message.content);
+        return await gpt.ask();
+      })
+    );
+
+    const answers = responses
+      .filter((response, index) => messages[index].role !== "system")
+      .map(response => response.content);
+
+    console.log(answers);
+
+    res.json({ answers });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Failed to ask ChatGPT API. Error: " + error,
+    });
+  }
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
