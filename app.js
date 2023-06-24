8c69f98fffcc2748a23ed52ce43e7d305351f861
Trid3r <alex_yhd@hotmail.com>
2023-06-23 18:49:18 -0600

    Update app.js 5

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require("express");
+import express from "express";
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3001;
-
-
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
