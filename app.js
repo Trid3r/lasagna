72975350312f1ded847951eb19247f17dc03a0f3
Trid3r <alex_yhd@hotmail.com>
2023-06-23 18:54:31 -0600

    Update app.js XD

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+// Imports
 import express from "express";
+import dotenv from "dotenv";
+import EasyGpt from "easygpt";
 
+// Constants
 dotenv.config();
-
-const app = express();
+// const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const port = process.env.PORT || 3001;
+const app = express();
+app.use(express.json());
 
+//Set GPT
+const gpt = new EasyGpt();
+
+// Run
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
