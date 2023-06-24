3b129496f443b5283169fe532b2fd2c223719995
Trid3r <alex_yhd@hotmail.com>
2023-06-23 18:47:51 -0600

    Update app.js 4

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-import express from "express";
-import dotenv from "dotenv";
-import EasyGpt from "easygpt";
-
-dotenv.config();
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-
+const express = require("express");
 const app = express();
-app.use(express.json());
+const port = process.env.PORT || 3001;
 
-const gpt = new EasyGpt();
+
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
