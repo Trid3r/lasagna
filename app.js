e4b4d4f7886ea15807a01f7fe4108a320c11bf4c
Trid3r <alex_yhd@hotmail.com>
2023-06-26 16:51:15 -0600

    Fix: Update main class

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import EasyGpt from "easygpt";
+import jwt from 'jsonwebtoken';
+import sqlite3 from 'sqlite3';
 
 // Constants
 dotenv.config();
@@ -20,39 +22,98 @@ gpt
     Ensure responses are grammatically correct and written in a professional yet conversational tone. Provide timely responses without significant delay.`
   )
   .addRule("Use emoticons in every answer and super often.")
-  // .addMessage("Hello! How are you")
   .advanced.setMaxTokens(100)
   .advanced.setTemperature(1.5);
 
+  //Set SQLite DB
+const db = new sqlite3.Database('./jwt.db');
+const createTableQuery = 'CREATE TABLE IF NOT EXISTS tokens (id INTEGER PRIMARY KEY AUTOINCREMENT, token TEXT)';
+db.run(createTableQuery, (err) => {
+  if (err) {
+    console.error('Error al crear la tabla:', err);
+  } else {
+    console.log('BD ready');
+  }
+});
+db.close();
 
-//Endpoint
-app.post("/askgpt", async (req, res) => {
-  const messages = req.body.messages;
-  
+//Function to validate token if was used and saved is not exist
+function tokenProcess(token) {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database('./jwt.db');
+    db.get('SELECT EXISTS(SELECT 1 FROM tokens WHERE token = ?) as tokenExist', [token], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        if(row.tokenExist){
+          resolve(false);
+        } else {
+          db.run('INSERT INTO tokens (token) VALUES (?)', [token], function (err) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(true);
+            }
+          });
+        }
+      }
+    });
+
+    db.close();
+  });
+}
+
+//Function to call chagpt
+async function handleChatLogic(res, messages) {
   try {
     const responses = await Promise.all(
-      messages.map(async message => {
+      messages.map(async (message) => {
         gpt.addMessage(message.content);
         return await gpt.ask();
       })
     );
 
     const answers = responses
-      .filter((response, index) => messages[index].role !== "system")
-      .map(response => response.content);
-
-    console.log(answers);
+      .filter((response, index) => messages[index].role !== 'system')
+      .map((response) => response.content);
 
     res.json({ answers });
   } catch (error) {
-    console.error(error);
+    return res
+      .status(401)
+      .json({ error: 'Failed to ask ChatGPT API. Error: ' + error });
+  }
+}
 
-    res.status(500).json({
-      error: "Failed to ask ChatGPT API. Error: " + error,
-    });
+//Endpoint
+app.post("/askgpt", async (req, res) => {
+  const messages = req.body.messages; // Obten el mensaje para chatGPT
+  const tokenWithBearer = req.headers['authorization']; // Obtén el token con 'Bearer' al principio
+  
+  if (!tokenWithBearer) { // Si no se proporcionó ningún token en el encabezado 'Authorization'
+    return res.status(401).json({ error: 'Token no proporcionado' });
   }
   
+  const token = tokenWithBearer.replace(/^Bearer\s+/, ''); // Elimina 'Bearer' y el espacio en blanco al principio
+
+  // Verificar y decodificar el token JWT
+  jwt.verify(token, 'BigBola', (err, decodedToken) => {
+    if (err) {
+      return res.status(401).json({ error: 'Token inválido' });
+    }
+
+    tokenProcess(token)
+    .then((tokenSaved) => {
+      if (!tokenSaved) return res.status(401).json({ error: 'Token usado' });
+      else handleChatLogic(res, messages);
+    })
+    .catch((err) => {
+      return res.status(401).json({ error: 'Error al guardar el token: '+err });
+    });
+
+  });
+
 });
 
 // Run
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`App running on port ${port}!`));
